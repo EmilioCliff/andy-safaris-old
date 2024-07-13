5cc52a43efbac2bc781acc39c543cb55a2a195ef
Emilio Cliff <emiliocliff@gmail.com>
2024-07-12 23:38:08 -0700

    remove cors in php

--- php/email.php
+++ php/email.php
@@ -1,8 +1,4 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST');
-    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
-
     $data = json_decode(file_get_contents("php://input"), true);
 
     if ($data) {
